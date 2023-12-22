058fba93118cf6116136e5b844975891bc0f69c0
Shanya <shanyaverma03@gmail.com>
2023-12-22 17:45:03 +0530

    Add logic

--- src/components/Test.js
+++ src/components/Test.js
@@ -2,8 +2,9 @@ import { useState } from "react";
 import "./Test.css";
 
 const Test = () => {
-  const [leftStyle, setLeftStyle] = useState(0);
-  const [topStyle, setTopStyle] = useState(0);
+  const [response, setResponse] = useState("");
+  const [leftStyle, setLeftStyle] = useState(null);
+  const [topStyle, setTopStyle] = useState(null);
 
   const handleButtonClick = () => {
     const button = document.getElementById("no");
@@ -28,19 +29,45 @@ const Test = () => {
 
   return (
     <div className="test-container">
-      <button
-        className="move-button"
-        onClick={handleButtonClick}
-        id="no"
-        style={{
-          position: "absolute",
-          top: topStyle,
-          left: leftStyle,
-          transition: "0.5s",
-        }}
-      >
-        Move me
-      </button>
+      <div className="header">
+        {response === "yes" ? (
+          <h1>Yipeeeee</h1>
+        ) : (
+          <h1>Do you wanna go out with me?</h1>
+        )}
+      </div>
+      <div className="gif">
+        {response === "yes" ? (
+          <img
+            src="https://media.giphy.com/media/lck9f0L8jPjRtkigYy/giphy.gif"
+            alt="please"
+          />
+        ) : (
+          <img
+            src="https://media.giphy.com/media/qA9BnxUPmgWgU/giphy.gif"
+            alt="please"
+          />
+        )}
+      </div>
+      {response !== "yes" && (
+        <div className="buttons-container">
+          <button className="btn" id="yes" onClick={() => setResponse("yes")}>
+            Yes
+          </button>
+          <button
+            className="btn no-button"
+            onClick={handleButtonClick}
+            id="no"
+            style={{
+              top: topStyle,
+              left: leftStyle,
+              transition: "0.5s",
+            }}
+          >
+            No
+          </button>
+        </div>
+      )}
     </div>
   );
 };
